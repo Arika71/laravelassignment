e1d6759570eac010b68739afdf102d9b93a9d96f
Arika <Karika21@tbc.edu.np>
2022-09-23 22:19:41 +0545

    laravelassignment

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+  <h1>Ekbana Internship</h1>
+
+
+    @foreach ($data as $value)
+        <h2> {{$value}}</h2>
+    @endforeach
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/post/{name}', [PostController::class, 'index']);
+
+ Route::get('/service/{name}/{address}', [ServiceController:: class, 'index']);
+
+
+ Route::resource('user', PostController::class)->except(['index', 'delete']);
+
+ Route::controller(PostController::class) ->group(function(){
+    Route::get('/index', 'index');
+    Route::get('/create', 'create');
+    Route::get('/store', 'store');
+    Route::get('/change', 'edit');
+     
 });
